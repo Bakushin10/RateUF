5bf16cba2eb9134700ff353b5e94a07b298b7d09
Shin <shnnagai@gmail.com>
2018-02-06 15:53:16 -0500

    resolve merge conflict

--- client/components/Professor.jsx
+++ client/components/Professor.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
-import { List, Avatar, Icon, Slider } from 'antd';
-import { Row, Grid, Col } from 'react-bootstrap';
-import InfiniteScroll from 'react-infinite-scroller';
-import Header from './New_Header';
-import Footer from './Footer';
-
-=======
 import { List, Avatar, Icon, Slider} from 'antd';
 import { Row, Grid, Col, DropdownButton, MenuItem,FormGroup, FormControl} from 'react-bootstrap';
 import InfiniteScroll from 'react-infinite-scroller';
 import styled from 'styled-components';
->>>>>>> 6f55004e9689074a7a0f8e3884858798446b714d
 import 'antd/dist/antd.css';
+import Header from './New_Header';
+import Footer from './Footer';
+
 
 
 const ProfessorName = styled.h5`
@@ -24,58 +18,10 @@ const ProfessorName = styled.h5`
 `
 
 class Professor extends React.Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      selectedMajor: '',
-      professor: [] //array professors
-    };
 
-<<<<<<< HEAD
-    this.getProfByMajor = this.getProfByMajor.bind(this);
-  }
-
-  componentDidMount() {
-    this.getProfByMajor(this, 'CS');
-    this.setState({ selectedMajor: 'CS' });
-  }
-
-  getProfByMajor(ev, major) {
-    axios
-      .get('/getProfByMajor?major=' + major) //passing major as an argument
-      .then(function(response) {
-        console.log('=== getProfByMajor === ');
-        console.log(response.data);
-        console.log(response.data[0].professor);
-        console.log('=== getProfByMajor === ');
-        ev.setState({ professor: response.data[0].professor });
-        ev.setState({ selectedMajor: response.data.major });
-      });
-  }
-
-  handleInfiniteOnLoad() {
-    let data = this.state.professor;
-    this.setState({
-      loading: true
-    });
-    if (data.length > 14) {
-      message.warning('Infinite List loaded all');
-      this.setState({
-        hasMore: false,
-        loading: false
-      });
-      return;
-    }
-    this.getData(res => {
-      data = data.concat(res.results);
-      this.setState({
-        data,
-        loading: false
-      });
-    });
-  }
-=======
         this.state = {
             selectedMajor: "CS",
             professor: [], //array professors to keep
@@ -88,7 +34,31 @@ class Professor extends React.Component {
         this.getProfByMajor = this.getProfByMajor.bind(this);
         this.handleSearchProf = this.handleSearchProf.bind(this);
         this.searchProf = this.searchProf.bind(this);
+    }   
+
+
+    handleInfiniteOnLoad() {
+        let data = this.state.professor;
+        this.setState({
+            loading: true
+        });
+        if (data.length > 14) {
+            message.warning('Infinite List loaded all');
+            this.setState({
+            hasMore: false,
+            loading: false
+            });
+            return;
+        }
+        this.getData(res => {
+            data = data.concat(res.results);
+            this.setState({
+            data,
+            loading: false
+            });
+        });
     }
+    
 
     componentDidMount(){
         this.getProfByMajor(this, 'CS');
@@ -130,7 +100,6 @@ class Professor extends React.Component {
             this.getProfByMajor(this, major);
         }
     }
->>>>>>> 6f55004e9689074a7a0f8e3884858798446b714d
 
   render() {
     const pagination = {
@@ -139,73 +108,6 @@ class Professor extends React.Component {
       total: this.state.professor.length,
       onChange: () => {}
     };
-
-<<<<<<< HEAD
-    const IconText = ({ type, text }) => (
-      <span>
-        <Icon type={type} style={{ marginRight: 8 }} />
-        {text}
-      </span>
-    );
-
-    console.log('this.state.selectedMajor');
-    console.log(this.state);
-    return (
-      <div className="container">
-        <Header />
-        <InfiniteScroll
-          initialLoad={false}
-          pageStart={0}
-          loadMore={this.handleInfiniteOnLoad}
-          hasMore={!this.state.loading && this.state.hasMore}
-          useWindow={false}
-        >
-          <List
-            itemLayout="vertical"
-            size="large"
-            pagination={pagination}
-            dataSource={this.state.professor}
-            renderItem={item => (
-              <Link to={`/ProfessorDetails/${item._id}`} {...item} param={{ id: item._id }}>
-                <Grid>
-                  <Row>
-                    <Col xs={12} md={4}>
-                      {item.name}
-                    </Col>
-                    <Col xs={7} md={6}>
-                      <Slider
-                        defaultValue={30}
-                        disabled={true}
-                        marks={{ 25: 'meh', 50: 'Ok', 75: 'good', 100: 'marry me!' }}
-                      />
-                    </Col>
-                  </Row>
-                </Grid>
-                <List.Item
-                  key={item.id}
-                  actions={[
-                    <IconText type="star-o" text="156" />,
-                    <IconText type="like-o" text="156" />,
-                    <IconText type="message" text="2" />
-                  ]}
-                >
-                  {item.content}
-                </List.Item>
-              </Link>
-            )}
-          />
-        </InfiniteScroll>
-        <Footer />
-      </div>
-    );
-  }
-=======
-        const pagination = {
-            pageSize: 10,
-            current: 1,
-            total: this.state.professorToShow.length,
-            onChange: (() => {}),
-        };
 
         const IconText = ({ type, text }) => (
             <span>
@@ -218,6 +120,7 @@ class Professor extends React.Component {
         
         return(
             <div className = "container">
+            <Header />
               <Grid>
                 <Col xs = {12} md = {3} className = "sidebar">{/* side bar*/}
                     <div>
@@ -294,10 +197,10 @@ class Professor extends React.Component {
                     </InfiniteScroll>
               </Col>
              </Grid>
+             <Footer />
             </div>
         )
     }
->>>>>>> 6f55004e9689074a7a0f8e3884858798446b714d
 }
 
 export default Professor;
--- client/components/ProfessorDetails.jsx
+++ client/components/ProfessorDetails.jsx
@@ -1,56 +1,12 @@
 import React from 'react';
 import axios from 'axios';
-<<<<<<< HEAD
-import { List, Avatar, Icon, Slider } from 'antd';
-=======
 import { List, Avatar, Icon, Slider} from 'antd';
 import { Row, Grid, Col, DropdownButton, MenuItem } from 'react-bootstrap';
 import { Button } from 'antd';
->>>>>>> 6f55004e9689074a7a0f8e3884858798446b714d
 
 import 'antd/dist/antd.css';
 
 class ProfessorDetails extends React.Component {
-<<<<<<< HEAD
-  constructor() {
-    super();
-
-    this.state = {
-      profName: '',
-      id: '',
-      prof: []
-    };
-  }
-
-  componentDidMount() {
-    let self = this;
-    const _id = this.props.match.params.id;
-    const name = this.props.match.params.name;
-    axios.get('/getProfDetails?_id=' + _id).then(function(response) {
-      self.inits(response.data.professor);
-    });
-  }
-
-  inits(array) {
-    array.forEach(element => {
-      if (element._id === this.props.match.params.id) {
-        this.setState({ profName: element.name });
-        this.setState({ id: element._id });
-      }
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>
-          {this.state.profName}
-          <div>{this.state.id}</div>
-        </h1>
-      </div>
-    );
-  }
-=======
     
     constructor(){
 
@@ -134,7 +90,6 @@ class ProfessorDetails extends React.Component {
             </div>
         )
     }
->>>>>>> 6f55004e9689074a7a0f8e3884858798446b714d
 }
 
 export default ProfessorDetails;
