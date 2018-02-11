10dcd1b172805b9e3e5214cde3c0222e97e8cbab
Shin <shnnagai@gmail.com>
2018-02-10 19:21:55 -0500

    done with creating states for both class/professor form

--- client/components/ClassForm.jsx
+++ client/components/ClassForm.jsx
@@ -1,19 +1,69 @@
 import React from 'react';
-
+import { Form, Select, Input, Slider, Icon, Button, Checkbox, Row, Col } from 'antd';
 import 'antd/dist/antd.css';
-
-import { Form, Select, Input, Slider, Icon, Button } from 'antd';
 import Header from './Header';
 import Footer from './Footer';
 
 const FormItem = Form.Item;
+const { TextArea } = Input;
 
 class ClassForm extends React.Component {
+
+    constructor(){
+        super();
+
+        this.state = {
+            overallExpe : 0,
+            levelOfDiffculty : 0,
+            extraComment: '',
+            knowBeforeCourse: ''
+        }
+
+        this.overAllExpeOnChange = this.overAllExpeOnChange.bind(this);
+        this.levelOfDiffcultyOnChange = this.levelOfDiffcultyOnChange.bind(this);
+        this.checkboxOnChange = this.checkboxOnChange.bind(this);
+        this.knowBeforeCourseOnChange = this.knowBeforeCourseOnChange.bind(this);
+        this.extraCommentOnChange = this.extraCommentOnChange.bind(this);
+        this.submitClicked = this.submitClicked.bind(this);
+    }
+
+    overAllExpeOnChange(value){
+        this.setState({overallExpe : value})
+        console.log("overallExpe")
+        console.log(this.state.overallExpe)
+    }
+
+    levelOfDiffcultyOnChange(value){
+        this.setState({levelOfDiffculty : value})
+        console.log("levelOfDiff")
+        console.log(this.state.levelOfDiffculty)
+    }
+
+    checkboxOnChange(value){
+        this.setState({a : value})
+        console.log("checkboxOnChange")
+    }
+
+    knowBeforeCourseOnChange(e){
+        this.setState({ knowBeforeCourse: e.target.value })
+        console.log(this.state.knowBeforeCourse)
+    }
+
+    extraCommentOnChange(e){
+        this.setState({ extraComment: e.target.value })
+        console.log(this.state.extraComment)
+    }
+
+    submitClicked(){
+        console.log("submitted");
+    }
+
     render() {
         const formItemLayout = {
             labelCol: {span: 8},
             wrapperCol : {span: 10}
         };
+
 
         return (
             <div className='button-center'>
@@ -21,70 +71,94 @@ class ClassForm extends React.Component {
                 <h1>Rate a Class</h1>
 
                 <div align="center">
-                    <Form.Item
-                        {...formItemLayout}
-                        label = "Select a Class"
-                        hasFeedback
-                    >
-                        <Select name="select" placeholder="Please select a Class">
-                            <Select.Option value="CDA 4630">CDA 4630 Embedded Systems</Select.Option>
-                            <Select.Option value="CIS 4914">CIS 4914 Senior Project</Select.Option>
-                            <Select.Option value="COP 3502">COP 3503 Programming 1</Select.Option>
-                        </Select>
-                    </Form.Item>
-                    <Form.Item
-                        {...formItemLayout}
-                        label="Overall Experience"
-                    >
-                        <Slider marks={{
-                            0: <div><Icon type="frown-o" style={{ fontSize: 15, color: '#db0f0f' }}/><div>meh</div></div>,
-                            50: <div><Icon type="meh-o"   style={{ fontSize: 15, color: '#08c' }}/><div>good</div></div>,
-                            100: <div><Icon type="smile-o" style={{ fontSize: 15, color: '#77f987' }}/><div>excellent</div></div>
-                        }} />
-                    </Form.Item>
-                    <Form.Item
-                        {...formItemLayout}
-                        label="Level of Difficulty"
-                    >
-                        <Slider marks={{
-                            0: <div><Icon type="frown-o" style={{ fontSize: 15, color: '#db0f0f' }}/><div>meh</div></div>,
-                            50: <div><Icon type="meh-o"   style={{ fontSize: 15, color: '#08c' }}/><div>good</div></div>,
-                            100: <div><Icon type="smile-o" style={{ fontSize: 15, color: '#77f987' }}/><div>excellent</div></div>
-                        }} />
-                    </Form.Item>
-                    <Form.Item
-                        {...formItemLayout}
-                        label="What to know before the course"
-                    >
-                        <Input />
-                    </Form.Item>
-                    <Form.Item
-                        {...formItemLayout}
-                        label="Extra Comments"
-                    >
-                        <Input />
-                    </Form.Item>
+                    <Form>
+                        <FormItem
+                            {...formItemLayout}
+                            label = "Select a Class"
+                            hasFeedback
+                        >
+                            <Select name="select" placeholder="Please select a Class" style={{ width: 150 }}>
+                                <Select.Option value="CDA 4630">CDA 4630 Embedded Systems</Select.Option>
+                                <Select.Option value="CIS 4914">CIS 4914 Senior Project</Select.Option>
+                                <Select.Option value="COP 3502">COP 3503 Programming 1</Select.Option>
+                            </Select>
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="Overall Experience"
+                        >
+                            <Slider
+                                onChange = {this.overAllExpeOnChange}
+                                value = {this.state.overallExpe}
+                                defaultValue={0}
+                                marks = {{
+                                    0: <div><Icon type="frown-o" style={{ fontSize: 15, color: '#db0f0f' }}/><div>meh</div></div>,
+                                    50: <div><Icon type="meh-o"   style={{ fontSize: 15, color: '#08c' }}/><div>good</div></div>,
+                                    100: <div><Icon type="smile-o" style={{ fontSize: 15, color: '#77f987' }}/><div>excellent</div></div>
+                                }} 
+                            />
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="Level of Difficulty"
+                        >
+                            <Slider
+                                onChange = {this.levelOfDiffcultyOnChange}
+                                value = {this.state.levelOfDiffculty}
+                                defaultValue =  {0}
+                                marks={{
+                                    0: <div><Icon type="frown-o" style={{ fontSize: 15, color: '#db0f0f' }}/><div>meh</div></div>,
+                                    50: <div><Icon type="meh-o"   style={{ fontSize: 15, color: '#08c' }}/><div>good</div></div>,
+                                    100: <div><Icon type="smile-o" style={{ fontSize: 15, color: '#77f987' }}/><div>excellent</div></div>
+                                }}
+                            />
+                        </FormItem>
+                        <FormItem  
+                            {...formItemLayout}
+                            label="What to know before the course"
+                        >
+                            <TextArea
+                                type = "text"
+                                value = {this.state.knowBeforeCourse}
+                                placeholder = "enter text"
+                                rows={4} 
+                                onChange = {this.knowBeforeCourseOnChange}
+                            />
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="Extra Comments"
+                        >
+                            <TextArea
+                                type = "text"
+                                value = {this.state.extraComment}
+                                placeholder = "enter text"
+                                rows={4} 
+                                onChange = {this.extraCommentOnChange}
+                            />
+                        </FormItem>
 
-                    {/*dont need this. Just showing another option for selecting something beforehand. */}
-                    {/*instead of the user typing, we have predetermined options and they are able to chose */}
-                    {/*whatever ones they want. (able to select as many as you want)*/}
-                    <FormItem
-                        {...formItemLayout}
-                        label="Select what to know beforehand"
-                    >
-                        <Select mode="multiple" placeholder="Please select what someone should
-                        know beforehand">
-                            <Select.Option value="C++">C++</Select.Option>
-                            <Select.Option value="Java">Java</Select.Option>
-                            <Select.Option value="Javascript">Javascript</Select.Option>
-                            <Select.Option value="Databases">Databases</Select.Option>
-                        </Select>
-                    </FormItem>
-                    <FormItem>
-                        <Button align="center" type="primary" htmlType="submit">
-                            Submit
-                        </Button>
-                    </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="Select what to know beforehand"
+                        >
+                            <Checkbox.Group style={{ width: '100%' }} onChange={this.checkboxOnChange}>
+                                <Row>
+                                    <Col span={8}><Checkbox value="Java">Java</Checkbox></Col>
+                                    <Col span={8}><Checkbox value="C++">C++</Checkbox></Col>
+                                    <Col span={8}><Checkbox value="C">C</Checkbox></Col>
+                                    <Col span={8}><Checkbox value="JavaScript">JavaScript</Checkbox></Col>
+                                    <Col span={8}><Checkbox value="Git">Git</Checkbox></Col>
+                                    <Col span={8}><Checkbox value="some sort of coding experiences">some sort of coding experiences</Checkbox></Col>
+                                </Row>
+                            </Checkbox.Group>
+                        </FormItem>
+                        <FormItem>
+                            <Button align="center" type="primary" htmlType="submit" onClick = {this.submitClicked}>
+                                Submit
+                            </Button>
+                        </FormItem>
+                    </Form>
                 </div>
                 <Footer />
             </div>
--- client/components/ProfessorForm.jsx
+++ client/components/ProfessorForm.jsx
@@ -1,109 +1,182 @@
 import React from 'react';
-
-
 import { Form, Select, Radio, Input, Slider, Icon, Rate, Button } from 'antd';
+
 import Header from './Header';
 import Footer from './Footer';
 
 const FormItem = Form.Item;
+const { TextArea } = Input;
 
 class ProfessorForm extends React.Component {
-  render() {
-      const formItemLayout = {
-          labelCol: {span: 8},
-          wrapperCol : {span: 9}
-      };
 
-      return (
-          <div className='button-center'>
-            <Header />
-            <h1>Rate a Professor</h1>
-                <div align="center">
-                    <Form.Item
-                        {... formItemLayout}
-                        label = "Select a Professor"
-                        hasFeedback
-                    >
-                        <Select name="select" placeholder="Please select a professor" style={{ width: 120 }} >
-                            <Select.Option value="Lisa Anthony">Lisa Anthony</Select.Option>
-                            <Select.Option value="David Small">David Small</Select.Option>
-                        </Select>
-                    </Form.Item>
-                    <Form.Item
-                        {...formItemLayout}
-                        label="Overall Experience"
-                    >
-                        <Slider marks={{
-                            0: <div><Icon type="frown-o" style={{ fontSize: 15, color: '#db0f0f' }}/><div>meh</div></div>,
-                            50: <div><Icon type="meh-o"   style={{ fontSize: 15, color: '#08c' }}/><div>good</div></div>,
-                            100: <div><Icon type="smile-o" style={{ fontSize: 15, color: '#77f987' }}/><div>excellent</div></div>
-                        }} />
-                    </Form.Item>
-                    <Form.Item
-                        {...formItemLayout}
-                        label="Level of Difficulty"
-                    >
-                        <Slider marks={{
-                            0: <div><Icon type="frown-o" style={{ fontSize: 15, color: '#db0f0f' }}/><div>meh</div></div>,
-                            50: <div><Icon type="meh-o"   style={{ fontSize: 15, color: '#08c' }}/><div>good</div></div>,
-                            100: <div><Icon type="smile-o" style={{ fontSize: 15, color: '#77f987' }}/><div>excellent</div></div>
-                        }} />
-                    </Form.Item>
-                    <Form.Item
-                        {...formItemLayout}
-                        label="Communication of Ideas"
-                    >
-                        <Slider marks={{
-                            0: <div><Icon type="frown-o" style={{ fontSize: 15, color: '#db0f0f' }}/><div>meh</div></div>,
-                            50: <div><Icon type="meh-o"   style={{ fontSize: 15, color: '#08c' }}/><div>good</div></div>,
-                            100: <div><Icon type="smile-o" style={{ fontSize: 15, color: '#77f987' }}/><div>excellent</div></div>
-                        }} />
-                    </Form.Item>
-                    <Form.Item
-                        {...formItemLayout}
-                        label="Facilitation of Learning"
-                    >
-                        <Slider marks={{
-                            0: <div><Icon type="frown-o" style={{ fontSize: 15, color: '#db0f0f' }}/><div>meh</div></div>,
-                            50: <div><Icon type="meh-o"   style={{ fontSize: 15, color: '#08c' }}/><div>good</div></div>,
-                            100: <div><Icon type="smile-o" style={{ fontSize: 15, color: '#77f987' }}/><div>excellent</div></div>
-                        }} />
-                    </Form.Item>
-                    <Form.Item
-                        {...formItemLayout}
-                        label="Would Take Again?"
-                    >
-                        <Select name="select" defaultValue="Yes" style={{ width: 80 }} >
-                            <Select.Option value="Yes">Yes</Select.Option>
-                            <Select.Option value="No">No</Select.Option>
-                        </Select>
-                    </Form.Item>
-                    <Form.Item
-                        {...formItemLayout}
-                        label="Extra Comments"
-                    >
-                        <Input />
-                    </Form.Item>
+    constructor(){
+        super();
+        this.state = {
+            profName: '',
+            overallExpe : 0,
+            levelOfDiffculty : 0,
+            communicationOfIdeas : 0,
+            facilitationOfLearning : 0,
+            wouldTakeAgain : "Yes", //by default
+            extraComment: '' 
+        }
 
-                    {/*don't need this. just showing another option for the form. */}
-                    {/*this is the stars that are shown.*/}
+        this.overAllExpeOnChange = this.overAllExpeOnChange.bind(this);
+        this.levelOfDiffcultyOnChange = this.levelOfDiffcultyOnChange.bind(this);
+        this.communicationOfIdeasOnChange = this.communicationOfIdeasOnChange.bind(this);
+        this.facilitationOfLearningOnChange = this.facilitationOfLearningOnChange.bind(this);
+        this.wouldTakeAgainOnChange = this.wouldTakeAgainOnChange.bind(this);
+        this.extraCommentOnChange = this.extraCommentOnChange.bind(this);
+        this.submitClicked = this.submitClicked.bind(this);
+    }
 
-                    <FormItem
-                        {...formItemLayout}
-                        label="Rate"
-                    >
-                        <Rate />
-                    </FormItem>
-                    <FormItem>
-                        <Button align="center" type="primary" htmlType="submit">
-                            Submit
-                        </Button>
-                    </FormItem>
-                </div>
-            <Footer />
-          </div>
-      )
-  }
+    overAllExpeOnChange(value){
+        this.setState({overallExpe : value})
+    }
+
+    levelOfDiffcultyOnChange(value){
+        this.setState({levelOfDiffculty : value})
+    }
+
+    communicationOfIdeasOnChange(value){
+        this.setState({communicationOfIdeas : value})
+    }
+
+    facilitationOfLearningOnChange(value){
+        this.setState({facilitationOfLearning : value})
+    }
+    wouldTakeAgainOnChange(value){
+        this.setState({wouldTakeAgain: value})
+    }
+
+    extraCommentOnChange(e){
+        this.setState({ extraComment: e.target.value })
+        console.log(this.state.extraComment)
+    }
+
+    submitClicked(){
+        console.log("submitted");
+    }
+
+    render() {
+        const formItemLayout = {
+            labelCol: {span: 8},
+            wrapperCol : {span: 9}
+        };
+
+        return (
+            <div className='button-center'>
+                <Header />
+                <h1>Rate a Professor</h1>
+                    <div align="center">
+                        <FormItem
+                            {... formItemLayout}
+                            label = "Select a Professor"
+                            hasFeedback
+                        >
+                            <Select name="select" placeholder="Please select a professor" style={{ width: 120 }} >
+                                <Select.Option value="Lisa Anthony">Lisa Anthony</Select.Option>
+                                <Select.Option value="David Small">David Small</Select.Option>
+                            </Select>
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="Overall Experience"
+                        >
+                            <Slider
+                                onChange = {this.overAllExpeOnChange}
+                                value = {this.state.overallExpe}
+                                defaultValue={0}
+                                marks = {{
+                                    0: <div><Icon type="frown-o" style={{ fontSize: 15, color: '#db0f0f' }}/><div>meh</div></div>,
+                                    50: <div><Icon type="meh-o"   style={{ fontSize: 15, color: '#08c' }}/><div>good</div></div>,
+                                    100: <div><Icon type="smile-o" style={{ fontSize: 15, color: '#77f987' }}/><div>excellent</div></div>
+                                }} 
+                            />
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="Level of Difficulty"
+                        >
+                            <Slider
+                                onChange = {this.levelOfDiffcultyOnChange}
+                                value = {this.state.levelOfDiffculty}
+                                defaultValue =  {0}
+                                marks={{
+                                    0: <div><Icon type="frown-o" style={{ fontSize: 15, color: '#db0f0f' }}/><div>meh</div></div>,
+                                    50: <div><Icon type="meh-o"   style={{ fontSize: 15, color: '#08c' }}/><div>good</div></div>,
+                                    100: <div><Icon type="smile-o" style={{ fontSize: 15, color: '#77f987' }}/><div>excellent</div></div>
+                                }} 
+                            />
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="Communication of Ideas"
+                        >
+                            <Slider
+                                onChange = {this.communicationOfIdeasOnChange}
+                                value = {this.state.communicationOfIdeas}
+                                defaultValue =  {0}
+                                marks={{
+                                    0: <div><Icon type="frown-o" style={{ fontSize: 15, color: '#db0f0f' }}/><div>meh</div></div>,
+                                    50: <div><Icon type="meh-o"   style={{ fontSize: 15, color: '#08c' }}/><div>good</div></div>,
+                                    100: <div><Icon type="smile-o" style={{ fontSize: 15, color: '#77f987' }}/><div>excellent</div></div>
+                                }} 
+                            />
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="Facilitation of Learning"
+                        >
+                            <Slider
+                                onChange = {this.facilitationOfLearningOnChange}
+                                value = {this.state.facilitationOfLearning}
+                                defaultValue = {0}
+                                marks={{
+                                    0: <div><Icon type="frown-o" style={{ fontSize: 15, color: '#db0f0f' }}/><div>meh</div></div>,
+                                    50: <div><Icon type="meh-o"   style={{ fontSize: 15, color: '#08c' }}/><div>good</div></div>,
+                                    100: <div><Icon type="smile-o" style={{ fontSize: 15, color: '#77f987' }}/><div>excellent</div></div>
+                                }} 
+                            />
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="Would Take This Professor Again?"
+                        >
+                            <Select name="select" defaultValue="Yes" style={{ width: 80 }} onChange = {this.wouldTakeAgainOnChange} >
+                                <Select.Option value = {"Yes"}> Yes </Select.Option>
+                                <Select.Option  value = {"No"}> No  </Select.Option>
+                            </Select>
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="Extra Comments"
+                        >
+                            <TextArea
+                                type = "text"
+                                value = {this.state.extraComment}
+                                placeholder = "enter text"
+                                rows={4} 
+                                onChange = {this.extraCommentOnChange}
+                            />
+                        </FormItem>
+
+                        <FormItem
+                            {...formItemLayout}
+                            label="Rate"
+                        >
+                            <Rate />
+                        </FormItem>
+                        <FormItem>
+                            <Button align="center" type="primary" htmlType="submit" onClick = {this.submitClicked}>
+                                Submit
+                            </Button>
+                        </FormItem>
+                    </div>
+                <Footer />
+            </div>
+        )
+    }
 }
 
   export default ProfessorForm;
