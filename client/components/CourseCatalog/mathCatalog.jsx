b61d6669e91ea75afdfed8a6a75ff18a5db2ef81
TDinaso559 <tdinaso@ufl.edu>
2018-02-19 18:00:19 -0500

    added the course catalogs

---
+++ client/components/CourseCatalog/mathCatalog.jsx
@@ -0,0 +1,297 @@
+import React from 'react';
+import { Table, Menu, Dropdown, Button } from 'antd';
+
+class mathCatalog extends React.Component {
+
+    render() {
+        const menu = (
+            <Menu>
+                <Menu.Item>
+                    <a target="_blank" rel="noopener noreferrer" href="./#/ComputerScienceEngineeringCatalog">CSE</a>
+                </Menu.Item>
+                <Menu.Item>
+                    <a target="_blank" rel="noopener noreferrer" href="./#/ComputerScienceLiberalArtsCatalog">CSC</a>
+                </Menu.Item>
+                <Menu.Item>
+                    <a target="_blank" rel="noopener noreferrer" href="./#/ComputerEngineeringCatalog">CE</a>
+                </Menu.Item>
+                <Menu.Item>
+                    <a target="_blank" rel="noopener noreferrer" href="./#/ElectricalEngineeringCatalog">EE</a>
+                </Menu.Item>
+                <Menu.Item>
+                    <a target="_blank" rel="noopener noreferrer" href="./#/MathCatalog">MATH</a>
+                </Menu.Item>
+            </Menu>
+        );
+        const semester1 = [{
+            title: 'Semester 1',
+            dataIndex: 'course',
+            key: 'course'
+        }, {
+            title: 'Credits',
+            dataIndex: 'credit',
+            key: 'credit'
+        }];
+
+        const semester2 = [{
+            title: 'Semester 2',
+            dataIndex: 'course',
+            key: 'course'
+        }, {
+            title: 'Credits',
+            dataIndex: 'credit',
+            key: 'credit'
+        }];
+
+        const semester3 = [{
+            title: 'Semester 3',
+            dataIndex: 'course',
+            key: 'course'
+        }, {
+            title: 'Credits',
+            dataIndex: 'credit',
+            key: 'credit'
+        }];
+
+        const semester4 = [{
+            title: 'Semester 4',
+            dataIndex: 'course',
+            key: 'course'
+        }, {
+            title: 'Credits',
+            dataIndex: 'credit',
+            key: 'credit'
+        }];
+
+        const semester5 = [{
+            title: 'Semester 5',
+            dataIndex: 'course',
+            key: 'course'
+        }, {
+            title: 'Credits',
+            dataIndex: 'credit',
+            key: 'credit'
+        }];
+
+        const semester6 = [{
+            title: 'Semester 6',
+            dataIndex: 'course',
+            key: 'course'
+        }, {
+            title: 'Credits',
+            dataIndex: 'credit',
+            key: 'credit'
+        }];
+
+        const semester7 = [{
+            title: 'Semester 7',
+            dataIndex: 'course',
+            key: 'course'
+        }, {
+            title: 'Credits',
+            dataIndex: 'credit',
+            key: 'credit'
+        }];
+
+        const semester8 = [{
+            title: 'Semester 8',
+            dataIndex: 'course',
+            key: 'course'
+        }, {
+            title: 'Credits',
+            dataIndex: 'credit',
+            key: 'credit'
+        }];
+
+        const data1 = [{
+            key: '1',
+            course: 'Biological or Physical Science',
+            credit: 3
+        }, {
+            key: '2',
+            course: 'Social and Behavioral Sciences',
+            credit: 3
+        }, {
+            key: '3',
+            course: 'MAC 2311 Analytic Geometry and Calculus 1',
+            credit: 4
+        }, {
+            key: '4',
+            course: 'IUF 1000 What is the Good Life\n' +
+            'GE-H',
+            credit: 3
+        }];
+
+        const data2 = [{
+            key: '1',
+            course: 'Biological or Physical Science',
+            credit: 4
+        }, {
+            key: '2',
+            course: 'Composition',
+            credit: 1
+        }, {
+            key: '3',
+            course: 'MAC 2312 Analytic Geometry and Calculus 2',
+            credit: 4
+        }, {
+            key: '4',
+            course: 'Elective',
+            credit: 3
+        }, {
+            key: '5',
+            course: 'Humanities',
+            credit: 3
+        }];
+
+        const data3 = [{
+            key: '1',
+            course: 'MHF 3202 Sets and Logic ',
+            credit: 3
+        }, {
+            key: '2',
+            course: 'MAC 2313 Analytic Geometry and Calculus 3' +
+            'GE-M',
+            credit: 4
+        }, {
+            key: '3',
+            course: 'Humanities',
+            credit: 3
+        }, {
+            key: '4',
+            course: 'Physical Science',
+            credit: 3
+        }, {
+            key: '5',
+            course: 'Science laboratory',
+            credit: 1
+        }, {
+            key: '5',
+            course: 'Social and Behavioral Sciences',
+            credit: 1
+        }];
+
+        const data4 = [{
+            key: '1',
+            course: 'MAP 2302 Elementary Differential Equations ',
+            credit: 3
+        }, {
+            key: '2',
+            course: 'MAS 4105 Linear Algebra 1',
+            credit: 4
+        }, {
+            key: '3',
+            course: 'Composition',
+            credit: 3
+        }, {
+            key: '4',
+            course: 'Biological Science',
+            credit: 3
+        }, {
+            key: '5',
+            course: 'Social and Behavioral Sciences',
+            credit: 3
+        }];
+
+        const data5 = [{
+            key: '1',
+            course: 'MAS 4301 Abstract Algebra 1',
+            credit: 3
+        }, {
+            key: '2',
+            course: 'Elective\n' +
+            '3000 level or higher, not in major',
+            credit: 3
+        }, {
+            key: '3',
+            course: 'Foreign language',
+            credit: 4
+        }, {
+            key: '4',
+            course: 'Mathematics elective',
+            credit: 3
+        }];
+
+        const data6 = [{
+            key: '1',
+            course: 'Elective',
+            credit: 3
+        }, {
+            key: '2',
+            course: 'Electives\n' +
+            'Two, at 3000 level or higher, not in major',
+            credit: 6
+        }, {
+            key: '3',
+            course: 'Foreign language',
+            credit: 4
+        }, {
+            key: '4',
+            course: 'Mathematics elective',
+            credit: 3
+        }];
+
+        const data7 = [{
+            key: '1',
+            course: 'MAA 4102 Introduction to Advanced Calculus \n'+
+            'for Engineers and Physical Scientists 1',
+            credit: 4
+        }, {
+            key: '2',
+            course: 'Elective\n' +
+            'Or foreign language if 4-3-3 option',
+            credit: 3
+        }, {
+            key: '3',
+            course: 'Electives\n' +
+            'Two, at 3000 level or higher, not in major',
+            credit: 6
+        }, {
+            key: '4',
+            course: 'Mathematics elective',
+            credit: 3
+        }];
+
+        const data8 = [{
+            key: '1',
+            course: 'MAA 4102 Introduction to Advanced Calculus\n' +
+            'for Engineers and Physical Scientists 2',
+            credit: 3
+        }, {
+            key: '2',
+            course: 'Elective',
+            credit: 3
+        }, {
+            key: '3',
+            course: 'Electives\n' +
+            'Two, at 3000 level or higher, not in major',
+            credit: 6
+        }, {
+            key: '4',
+            course: 'Mathematics elective',
+            credit: 3
+        }];
+
+
+        return (
+
+            <div>
+                <Dropdown overlay = {menu} title="Change Major">
+                    <Button >Change Degree Catalog</Button>
+                </Dropdown>
+                <Table align="center" width="500" columns={semester1} dataSource={data1} />
+                <Table align="center" width="500" columns={semester2} dataSource={data2} />
+                <Table align="center" width="500" columns={semester3} dataSource={data3} />
+                <Table align="center" width="500" columns={semester4} dataSource={data4} />
+                <Table align="center" width="500" columns={semester5} dataSource={data5} />
+                <Table align="center" width="500" columns={semester6} dataSource={data6} />
+                <Table align="center" width="500" columns={semester7} dataSource={data7} />
+                <Table align="center" width="500" columns={semester8} dataSource={data8} />
+
+
+            </div>
+        )
+    }
+}
+
+export default mathCatalog;
--- client/routes.js
+++ client/routes.js
@@ -7,20 +7,33 @@ import Professor from './components/Professor';
 import ProfessorDetails from './components/ProfessorDetails';
 import Course from './components/Course';
 import CourseDetails from './components/CourseDetails';
-import ClassForm from './components/ClassForm';
-import FourOhFour from './components/FourOhFour';
+import cseCatalog from './components/CourseCatalog/cseCatalog';
+import cscCatalog from './components/CourseCatalog/cscCatalog';
+import ceCatalog from './components/CourseCatalog/ceCatalog';
+import eeCatalog from './components/CourseCatalog/eeCatalog';
+import mathCatalog from './components/CourseCatalog/mathCatalog';
 
 export const Routes = () => (
     <Switch>
-      <Route exact path='/' component={Home} />
-      <Route path='/Professor' component={Professor} />
-      <Route path='/ProfessorDetails/:major/:id/:name' component={ProfessorDetails} />
-      <Route path='/ProfessorForm/:major/:profName' component={ProfessorForm} />
-      <Route path='/Class' component={Course} />
-      <Route path='/ClassDetails/:major/:id/:courseCode' component={CourseDetails} />
-      <Route path='/ClassForm/:major/:courseCode/:courseName' component={ClassForm} />
-      <Route component={FourOhFour} />
+          <Route exact path='/' component={Home} />
+          <Route path='/Professor' component={Professor} />
+          <Route path='/ProfessorDetails/:major/:id/:name' component={ProfessorDetails} />
+          <Route path='/ProfessorForm/:major/:profName' component={ProfessorForm} />
+          <Route path='/Class' component={Course} />
+          <Route path='/ClassDetails/:major/:id/:courseCode' component={CourseDetails} />
+          <Route path='/ClassForm/:major/:courseCode/:courseName' component={ClassForm} />
+
+          <Route path='/ComputerScienceEngineeringCatalog' component={cseCatalog} />
+          <Route path='/ComputerScienceLiberalArtsCatalog' component={cscCatalog} />
+          <Route path='/ComputerEngineeringCatalog' component={ceCatalog} />
+          <Route path='/ElectricalEngineeringCatalog' component={eeCatalog} />
+          <Route path='/MathCatalog' component={mathCatalog} />
+
+          <Route component={FourOhFour} />
     </Switch>
 );
+import ClassForm from './components/ClassForm';
+
+import FourOhFour from './components/FourOhFour';
 
 export default Routes;
